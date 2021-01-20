8a0f0cae92dddf48ddc27fbb793728e898629d6b
kenta-nishimoto <kenta.nishimoto@di-v.co.jp>
2021-01-20 17:15:01 +0900

    フォームオブジェクトの準備とコントローラーの定義

--- app/models/pay_form.rb
+++ app/models/pay_form.rb
@@ -1,1 +1,11 @@
 # これは購入機能のformオブジェクトのモデル
+
+class PayForm
+  include ActiveModel::Model
+  #Active Recordの場合と同様に form_with や render などのヘルパーメソッドが使用できるようになる
+
+ attr_accessor  :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id ,:item_id
+  # フォームオブジェクトで使用する値を定義している（読み書きができるようになっている）
+
+
+end
