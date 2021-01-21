430466e1f2023d3825c0c54e3a62d16f5405558a
kenta-nishimoto <kenta.nishimoto@di-v.co.jp>
2021-01-21 09:28:18 +0900

    フォームを保存できるように実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,5 +4,29 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order = PayForm.new
   end
+
+  def create
+    @order = PayForm.new(order_params)
+    if @order.valid? #バリデーションチェック
+      @order.save #trueなら、フォームオブジェクトのsaveメソッドの呼び出し
+      redirect_to root_path #処理後はリダイレクト
+    else
+      @item = Item.find(params[:item_id]) # indexで必要な@itemを再定義
+      render 'index' # indexの再描写
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:pay_form).permit(
+      :postal_code,
+      :prefecture_id,
+      :city,
+      :address,
+      :building,
+      :phone_number
+      ).merge(user_id: current_user.id,item_id: params[:item_id])
+  end
   
 end
